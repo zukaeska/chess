18f9d106bc21b1b822e194b2de6b442e352ff814
zukaeska <84535983+zukaeska@users.noreply.github.com>
2021-06-13 22:52:53 +0400

    Add files via upload

---
+++ content/script/positionsGame3.js
@@ -0,0 +1,17 @@
+var index;
+window.onload = function(){
+    var mainObject = JSON.parse(game3);
+    positioning(mainObject);
+}
+function positioning(object){
+    var quantity = object.positions.quantity;
+    for(index = 0; index < quantity; index++){
+        var pieceName = object.positions.pieces[index].name;
+        var piecePosition = object.positions.pieces[index].position;
+        var parent = document.getElementById(piecePosition);
+        var child = document.createElement("img");
+        child.src = "../images/" + pieceName +".png";
+        child.className = pieceName;
+        parent.appendChild(child);
+   }
+}
